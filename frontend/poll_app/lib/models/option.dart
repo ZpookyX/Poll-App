0452e5c422e2f8d9cb716da424fcda953bd51338
Lucas <lucas@Lucas-Macbook.lan>
2025-05-23 10:18:38 +0200

    Git error - try to fix

--- frontend/poll_app/lib/models/option.dart
+++
@@ -1,9 +0,0 @@
-class Option {
-  Option(this.id, this.text, this.votes);
-  final int id;
-  final String text;
-  final int votes;
-
-  factory Option.fromJson(Map<String, dynamic> j) =>
-      Option(j['option_id'], j['option_text'], j['votes']);
-}
