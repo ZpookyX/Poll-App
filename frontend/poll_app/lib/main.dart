345b87a07e8191cb1b1836ef6accaf3b842e0dd3
Lucas <lucas@Lucas-Macbook.lan>
2025-05-23 10:28:35 +0200

    Maybe final git fix/project structure fix

--- frontend/poll_app/lib/main.dart
+++ frontend/poll_app/lib/main.dart
@@ -3,10 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:poll_app/provider/poll_provider.dart';
 import 'package:provider/provider.dart';
-import 'routes/app_router.dart';
-import 'provider/auth_provider.dart';
-import 'provider/theme_provider.dart';
-import 'provider/profile_provider.dart';
+import '../../lib/routes/app_router.dart';
+import '../../lib/provider/auth_provider.dart';
+import '../../lib/provider/theme_provider.dart';
+import '../../lib/provider/profile_provider.dart';
 void main() => runApp(
   MultiProvider(
     providers: [
